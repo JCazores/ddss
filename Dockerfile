d21f74e7f5502ac035be86b6fb00147ec175be24
JCazores <john.christian.azores2016@gmail.com>
2026-06-07 19:10:28 +0800

    Simplify to port 80

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM php:8.2-apache
 RUN docker-php-ext-install mysqli pdo pdo_mysql
 COPY . /var/www/html/
-COPY start.sh /start.sh
-RUN chmod +x /start.sh
 RUN chown -R www-data:www-data /var/www/html
 EXPOSE 80
-CMD ["/start.sh"]
+ENV APACHE_RUN_DIR /var/run/apache2
+CMD ["apache2-foreground"]
--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-echo "Listen ${PORT:-80}" > /etc/apache2/ports.conf
-echo "<VirtualHost *:${PORT:-80}>" > /etc/apache2/sites-enabled/000-default.conf
-echo "DocumentRoot /var/www/html" >> /etc/apache2/sites-enabled/000-default.conf
-echo "</VirtualHost>" >> /etc/apache2/sites-enabled/000-default.conf
 exec apache2-foreground
