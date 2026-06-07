ee9d375da01cf93195c48afe090bdb33fcde8451
JCazores <john.christian.azores2016@gmail.com>
2026-06-07 18:48:07 +0800

    Connect DDSS to Railway MySQL

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,19 +1,18 @@
 <?php
-//error_reporting(0);
 ob_start();
 session_start();
-$siteName = "Drop-Out Deciscion Support System.com";
+$siteName = "Drop-Out Decision Support System";
 
-//DEFINE("BASE_URL","http://cipetbhopal.com/");
-DEFINE("BASE_URL","http://localhost/DDSS/");
+DEFINE("BASE_URL", "https://ddss-production.up.railway.app/");
 
-DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PSWD', ''); 
-DEFINE ('DB_HOST', 'localhost'); 
-DEFINE ('DB_NAME', 'ddss'); 
+DEFINE('DB_USER', 'root');
+DEFINE('DB_PSWD', 'oCZnrPaBlUHwYSSosvPTWAFRnKiSwQJI');
+DEFINE('DB_HOST', 'acela.proxy.rlwy.net');
+DEFINE('DB_PORT', '58509');
+DEFINE('DB_NAME', 'railway');
 
-date_default_timezone_set('Asia/Calcutta'); 
-$conn =  new mysqli(DB_HOST,DB_USER,DB_PSWD,DB_NAME);
+date_default_timezone_set('Asia/Manila');
+$conn = new mysqli(DB_HOST, DB_USER, DB_PSWD, DB_NAME, DB_PORT);
 if($conn->connect_error)
-die("Failed to connect database ".$conn->connect_error );
+    die("Failed to connect database " . $conn->connect_error);
 ?>
